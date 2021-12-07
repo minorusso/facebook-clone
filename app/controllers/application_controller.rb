c8f1a34818fc37e3470fe7f410c4c9df0fb9dd46
minorusso <gumbomb1@gmail.com>
2021-12-07 16:30:41 +0900

    ログイン時アクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   include SessionsHelper
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
   end
 end
