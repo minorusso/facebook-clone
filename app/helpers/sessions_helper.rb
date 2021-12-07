3a7c7449a539a2ad78e10188a53bc45d8ddd04fd
minorusso <gumbomb1@gmail.com>
2021-12-07 16:12:38 +0900

    ログインとログアウト機能追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
-class ApplicationController < ActionController::Base
-  include SessionsHelper
+module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
   end
 end
